64244221a80ce2083c93138e61b439e13396aee3
Mathieu SALLIOT <mathieu.salliot@epita.fr>
2023-03-28 19:29:21 +0200

    Lancecment du Docker + UDPDos dessus

--- weka-docker/docker-app/src/main/java/org/example/dockerapp/App.java
+++ weka-docker/docker-app/src/main/java/org/example/dockerapp/App.java
@@ -1,47 +1,93 @@
 package org.example.dockerapp;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.io.IOUtils;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.async.ResultCallbackTemplate;
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.model.Frame;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.model.ContainerNetwork;
 import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.core.command.LogContainerResultCallback;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) throws InterruptedException, IOException {
+        // Data
+        String imageId = "fersuy/contackgen-ubuntu:22.04";
+        String containerName = "contackgen-ubuntu";
+        String containerFile = "/data/capture.pcap";
+        String hostFile = "./capture.pcap";
 
-        // Create a Docker client using the default configuration
-        DockerClient dockerClient = DockerClientBuilder
-                .getInstance(DefaultDockerClientConfig.createDefaultConfigBuilder().build()).build();
+        // Docker client
+        DockerClient dockerClient = DockerClientBuilder.getInstance().build();
 
-        // create a new container from the hello-world image
-        CreateContainerResponse container = dockerClient.createContainerCmd("fersuy/contackgen-ubuntu:22.04")
-                .withName("hello-world-container")
-                .exec();
+        // Create container
+        try (CreateContainerCmd createContainer = dockerClient
+                .createContainerCmd(imageId).withName(containerName)) {
+            createContainer.withTty(true);
+            createContainer.exec();
+        }
 
-        // start the container
-        dockerClient.startContainerCmd(container.getId()).exec();
+        // Start container
+        dockerClient.startContainerCmd(containerName).exec();
 
-        // wait for the container to finish and redirect the output to the console
-        dockerClient.logContainerCmd(container.getId())
-                .withStdOut(true)
-                .withStdErr(true)
-                .exec(new ResultCallbackTemplate<LogContainerResultCallback, Frame>() {
-                    @Override
-                    public void onNext(Frame frame) {
-                        System.out.print(new String(frame.getPayload()));
-                    }
-                }).awaitCompletion(30, TimeUnit.SECONDS);
+        // Get Ip address
+        ContainerNetwork network = dockerClient.inspectContainerCmd(containerName).exec().getNetworkSettings()
+                .getNetworks().values().iterator().next();
+        String ipAddress = network.getIpAddress();
+        System.out.println("IP Address: " + ipAddress);
+        UDPDos udp = new UDPDos(ipAddress);
+        udp.start();
 
-        // // stop and remove the container
-        dockerClient.removeContainerCmd(container.getId()).exec();
+        // Sleep 70 seconds
+        Thread.sleep(70000);
+
+        // Copy file from container
+        try (TarArchiveInputStream tarStream = new TarArchiveInputStream(
+                dockerClient.copyArchiveFromContainerCmd(containerName,
+                        containerFile).exec())) {
+            unTar(tarStream, new File(hostFile));
+        }
+
+        // Stop container
+        dockerClient.killContainerCmd(containerName).exec();
+
+        // Remove container
+        dockerClient.removeContainerCmd(containerName).exec();
+    }
+
+    public static void unTar(TarArchiveInputStream tis, File destFile)
+            throws IOException {
+        TarArchiveEntry tarEntry = null;
+        while ((tarEntry = tis.getNextTarEntry()) != null) {
+            if (tarEntry.isDirectory()) {
+                if (!destFile.exists()) {
+                    destFile.mkdirs();
+                }
+            } else {
+                FileOutputStream fos = new FileOutputStream(destFile);
+                IOUtils.copy(tis, fos);
+                fos.close();
+            }
+        }
+        tis.close();
     }
 
 }
+
+// wait for the container to finish and redirect the output to the console
+// dockerClient.logContainerCmd(container.getId())
+// .withStdOut(true)
+// .withStdErr(true)
+// .exec(new ResultCallbackTemplate<LogContainerResultCallback, Frame>() {
+// @Override
+// public void onNext(Frame frame) {
+// System.out.print(new String(frame.getPayload()));
+// }
+// }).awaitCompletion(30, TimeUnit.SECONDS);
